f62269e5509977b5e6f4c05aff1a89223e4e750a
Gabriela <gabassegio@gmail.com>
2023-09-13 11:18:03 -0300

    Improved color naming funcion(Specific colors) & started debugging possible exceptions(No person on frame)

--- features_pkg/src/feature_recognition.py
+++ features_pkg/src/feature_recognition.py
@@ -11,9 +11,51 @@ import rospy
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image as imgmsg
 from features_pkg.srv import Features
+import webcolors
+from collections import namedtuple
+import sys
 
 
-PATH = '/home/robofei/Workspace/catkin_ws/src/3rd_party/vision_system/Feature_extraction/features_pkg/src'
+#PATH = '/home/robofei/Workspace/catkin_ws/src/3rd_party/vision_system/Feature_extraction/features_pkg/src'
+PATH = '/home/bibo/catkin_fodase/src/Feature_extraction/features_pkg/src'
+
+NAMED_COLORS = {
+    'red': (255, 0, 0),
+    'green': (0, 128, 0),
+    'blue': (0, 0, 255),
+    'yellow': (255, 255, 0),
+    'orange': (255, 165, 0),
+    'purple': (128, 0, 128),
+    'pink': (255, 192, 203),
+    'brown': (165, 42, 42),
+    'white': (255, 255, 255),
+    'black': (0, 0, 0),
+    'gray': (128, 128, 128),
+    'cyan': (0, 255, 255),
+    'magenta': (255, 0, 255),
+    'lime': (0, 255, 0),
+    'teal': (0, 128, 128),
+    'lavender': (230, 230, 250),
+    'maroon': (128, 0, 0),
+    'olive': (128, 128, 0),
+    'navy': (0, 0, 40),
+    'silver': (192, 192, 192),
+    'gold': (255, 215, 0),
+    'violet': (238, 130, 238),
+    'turquoise': (64, 224, 208),
+    'beige': (245, 245, 220),
+    'salmon': (250, 128, 114),
+    'khaki': (240, 230, 140),
+    'indigo': (75, 0, 130),
+    'coral': (255, 127, 80),
+    'orchid': (218, 112, 214),
+    'tan': (210, 180, 140),
+    'azure': (240, 255, 255),
+    'sienna': (160, 82, 45),
+    'crimson': (220, 20, 60)
+}
+
+
 
 class FeaturesRecognition:
 
@@ -24,62 +66,39 @@ class FeaturesRecognition:
         project = rf.workspace().project("mask-detection-m3skq")
         self.maskModel = project.version(1).model
         self.bridge = CvBridge()
-        self.topic = "/camera/rgb/image_raw"
+        #self.topic = "/camera/rgb/image_raw"
+        self.topic = '/usb_cam/image_raw'
         self.rate = rospy.Rate(5)
         self.img_sub = rospy.Subscriber(self.topic,imgmsg,self.camera_callback)
-        time.sleep(1)
+        time.sleep(0.5)
         rospy.Service('features', Features, self.handler)
-
 
 
     def camera_callback(self,data):
         self.cam_image = data
 
 
-    def handler(self, request):
-        self.recog = 0
-        rospy.loginfo("Service called!")
-        rospy.loginfo("Requested..")
-        time.sleep(2)
-        while self.recog == 0:
-            self.img_sub = rospy.Subscriber(self.topic,imgmsg,self.camera_callback)
-            out  = self.features()
-            self.rate.sleep()
-            return out
-        cv2.destroyAllWindows()
+    def funCrop(self):
+        result = None
+        while result is None:
+            frame = self.bridge.imgmsg_to_cv2(self.cam_image,desired_encoding='bgr8')
+            rospy.logwarn('Person not found... Trying again')
+            results = self.personModel.predict(frame)[0]
+            am = -100 
+            for i, box in enumerate(results.boxes):
+                if box.cls.item() == 0:
+                    cords = box.xyxy[0].tolist()
+                    area = (cords[2] - cords[0]) * (cords[3] - cords[1])
+                    if area > am:
+                        am = area
+                        xyxy = cords
+            if 'xyxy' in locals():
+                result = results[0]
 
-
-    def features(self):
-        frame = self.bridge.imgmsg_to_cv2(self.cam_image,desired_encoding='bgr8')
-        time.sleep(1)
         rospy.loginfo('Frame set')
         cv2.imwrite(PATH + '/data/frame.jpg', frame)    
-        
-        self.funCrop()
-        pantscolor = self.funColor(PATH + '/data/pants.jpg')
-        shirtcolor = self.funColor(PATH + '/data/shirt.jpg')
-        glasses = self.ifGlasses(PATH + '/data/head.jpg')
-        mask = self.ifMask(PATH + '/data/head.jpg')
 
-        out= f'I really like your {pantscolor} pants and your {shirtcolor} shirt! I see youre {glasses} wearing glasses. And youre {mask} wearing a mask.'
-
-        return out
-
-    def funCrop(self):
-        img = cv2.imread(PATH + '/data/frame.jpg')
-        results = self.personModel.predict(img)
-
-        result = results[0]  
-        am = -100 
-        for i, box in enumerate(result.boxes):
-            if box.cls.item() == 0: 
-                cords = box.xyxy[0].tolist()
-                area = (cords[2] - cords[0]) * (cords[3] - cords[1])
-                if area > am:
-                    am = area
-                    xyxy = cords
-        crop = img[int(xyxy[1]):int(xyxy[3]),int(xyxy[0]):int(xyxy[2])]       
-        #cv2.imwrite('/home/bibo/catkin_fodase/src/features/src/crop.jpg',crop)
+        crop = frame[int(xyxy[1]):int(xyxy[3]),int(xyxy[0]):int(xyxy[2])]       
         self.funKeypoints(crop)
 
 
@@ -91,67 +110,68 @@ class FeaturesRecognition:
         results = self.keypointsModel(img) 
         result = results[0]
         result_keypoint = result.keypoints.xy.cpu().numpy()[0]
-        #result_keypoint = result.cpu.keypoints.xy.numpy()[0]
-        print(result_keypoint)
         ombro = int(result_keypoint[5][1])
         quadril = int(result_keypoint[11][1])
         nareba = int(result_keypoint[0][0])      
 
-        '''        
-        cv2.circle(img,(nareba,ombro),5,(255,0,0),10)
-        cv2.circle(img,(nareba,quadril),5,(0,255,0),10)
-        cv2.imwrite(PATH +'/data/keypoints.jpg',img)
-        '''
-
-        shirt = img[ombro:quadril-30,20:-20]
-        pants = img[quadril+20:,:]
+        shirt = img[ombro+10:quadril-40,30:-30]
+        pants = img[quadril+25:,:]
         height, width = pants.shape[:2] 
-        pants = pants[:int(height/2),50:int(width/2)]
+        pants = pants[:int(height/3),30:int(width/2)+10]
         head = img[:ombro,10:-10]
         head = self.zoom(head,3)
         cv2.imwrite(PATH +'/data/head.jpg',head)
         cv2.imwrite(PATH +'/data/pants.jpg',pants)
         cv2.imwrite(PATH +'/data/shirt.jpg',shirt)
+        self.saturation(PATH + '/data/shirt.jpg')
+
 
     def saturation(self,path):
         image = cv2.imread(path)
         hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-        saturation_scale = 2.5
+        saturation_scale = 2.2
         hsv_image[:, :, 1] = np.clip(hsv_image[:, :, 1] * saturation_scale, 0, 255).astype(np.uint8)
         img = cv2.cvtColor(hsv_image, cv2.COLOR_HSV2BGR)
         cv2.imwrite(path, img)
-         
+
+
+    def color_distance(self, color1, color2):
+        r1, g1, b1 = color1
+        r2, g2, b2 = color2
+        return ((r1 - r2) ** 2 + (g1 - g2) ** 2 + (b1 - b2) ** 2) ** 0.5
+
+
+    def closest_color(self,requested_color):
+        min_distance = sys.maxsize
+        closest_name = None
+        for name, color in NAMED_COLORS.items():
+            distance = self.color_distance(requested_color, color)
+            if distance < min_distance:
+                min_distance = distance
+                closest_name = name
+        return closest_name
+
+
+    def rgb_to_color_name(self,rgb):
+        try:
+            # Normalizar os valores RGB para o intervalo [0, 255]
+            r, g, b = [int(x) for x in rgb]
+            # Encontrar a cor mais próxima na lista predefinida
+            color_name = self.closest_color((r, g, b))
+            return color_name
+        except ValueError:
+            return "Cor inválida"            
+            
+
 
     def funColor(self,path):
-        self.saturation(path)
         color_thief = ColorThief(path)
         dominant_color = color_thief.get_color()
-        R = dominant_color[0]
-        G = dominant_color[1]
-        B = dominant_color[2]
-        print(R,G,B)
+        print(dominant_color)
+        #cor = self.findColor(dominant_color)
+        #cor = self.convert_rgb_to_names(dominant_color)
+        cor = self.rgb_to_color_name(dominant_color)
 
-
-        if R >= 100 and R <= 255 and G >= 0 and G <= 80 and B >= 0 and B <= 80:
-            cor = 'Red'
-        elif R >= 0 and R <= 80 and G >= 100 and G <= 255 and B >= 0 and B <= 80:
-            cor = 'Green'
-        elif R >= 0 and R <= 80 and G >= 0 and G <= 80 and B >= 100 and B <= 255:
-            cor = 'Blue'
-        elif R >= 200 and R <= 255 and G >= 200 and G <= 255 and B >= 0 and B <= 50:
-            cor = 'Yellow'
-        elif R >= 90 and R <= 150 and G >= 0 and G <= 50 and B >= 90 and B <= 150:
-            cor = 'Pink'
-        elif R >= 200 and R <= 255 and G >= 90 and G <= 150 and B >= 0 and B <= 50:
-            cor = 'Orange'
-        elif R >= 200 and R <= 255 and G >= 121 and G <= 150 and B >= 121 and B <= 150:
-            cor = 'Pink'
-        elif R >= 0 and R <= 31 and G >= 0 and G <= 31 and B >= 0 and B <= 31:
-            cor = 'Gray'
-        elif R >= 151 and R <= 255 and G >= 151 and G <= 255 and B >= 151 and B <= 255:
-            cor = 'White'
-        else:
-            cor = 'Gray'
         return cor
         
 
@@ -187,6 +207,7 @@ class FeaturesRecognition:
         else:
             return 'Not' 
 
+
     def ifMask(self,path):
         try:
             results = self.maskModel.predict(path, confidence=9, overlap=30).json()
@@ -196,6 +217,33 @@ class FeaturesRecognition:
                 return 'Not'
         except:
             return 'Not' #Mais provável da pessoa estar sem máscara !!!!!!! TESTAR !!!!!!!
+
+
+    def features(self):
+
+        self.funCrop()
+        pantscolor = self.funColor(PATH + '/data/pants.jpg')
+        shirtcolor = self.funColor(PATH + '/data/shirt.jpg')
+        glasses = self.ifGlasses(PATH + '/data/head.jpg')
+        mask = self.ifMask(PATH + '/data/head.jpg')
+
+        out= f'I really like your {pantscolor} pants and your {shirtcolor} shirt! I see youre {glasses} wearing glasses. And youre {mask} wearing a mask.'
+
+        return out
+
+
+    def handler(self, request):
+            self.recog = 0
+            rospy.loginfo("Service called!")
+            rospy.loginfo("Requested..")
+            while self.recog == 0:
+                self.img_sub = rospy.Subscriber(self.topic,imgmsg,self.camera_callback)
+                out  = self.features()
+                self.rate.sleep()
+                return out
+            cv2.destroyAllWindows()
+
+
 
 if __name__ == "__main__":
     rospy.init_node('feature_bonus', log_level=rospy.INFO)
