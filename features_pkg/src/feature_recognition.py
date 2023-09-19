6ab56af877ed66201d59089651559711ab4c4932
Gabriela <gabassegio@gmail.com>
2023-09-19 16:58:29 -0300

    Added glasses and mask detectors, fixed color codes. Final tests initiated.

--- features_pkg/src/feature_recognition.py
+++ features_pkg/src/feature_recognition.py
@@ -15,40 +15,42 @@ import webcolors
 from collections import namedtuple
 import sys
 
+DEVICE = 'Bibo' #'Hera'
 
-#PATH = '/home/robofei/Workspace/catkin_ws/src/3rd_party/vision_system/Feature_extraction/features_pkg/src'
-PATH = '/home/bibo/catkin_fodase/src/Feature_extraction/features_pkg/src'
+if DEVICE == 'Hera':
+    PATH = '/home/robofei/Workspace/catkin_ws/src/3rd_party/vision_system/Feature_extraction/features_pkg/src'
+    TOPIC = '/camera/rgb/image_raw'
+else:
+    PATH = '/home/bibo/catkin_fodase/src/Feature_extraction/features_pkg/src'
+    TOPIC = '/usb_cam/image_raw' 
 
 NAMED_COLORS = {
     'red': (255, 0, 0),
     'green': (0, 128, 0),
     'blue': (0, 0, 255),
     'yellow': (255, 255, 0),
-    'orange': (255, 165, 0),
+    'orange': (255, 140, 0),  # Adjusted orange
     'purple': (128, 0, 128),
-    'pink': (255, 192, 203),
-    'brown': (165, 42, 42),
-    'white': (255, 255, 255),
-    'black': (0, 0, 0),
-    'gray': (128, 128, 128),
+    'brown': (139, 69, 19),  # Adjusted brown
+    'white': (208, 206, 223),  # Adjusted white
+    'whitee':(135, 135, 136),
+    'black': (20, 20, 20),
+    'blackk': (40, 40, 40),
+    'gray': (128, 128, 128),  # Wider range of gray
     'cyan': (0, 255, 255),
     'magenta': (255, 0, 255),
     'lime': (0, 255, 0),
-    'teal': (0, 128, 128),
-    'lavender': (230, 230, 250),
     'maroon': (128, 0, 0),
     'olive': (128, 128, 0),
-    'navy': (0, 0, 40),
-    'silver': (192, 192, 192),
+    'navy': (5, 5, 40),  # Wider range of navy
     'gold': (255, 215, 0),
     'violet': (238, 130, 238),
-    'turquoise': (64, 224, 208),
+    'turquoise': (64, 224, 208),  # Adjusted turquoise
     'beige': (245, 245, 220),
     'salmon': (250, 128, 114),
     'khaki': (240, 230, 140),
-    'indigo': (75, 0, 130),
+    'indigo': (75, 0, 130),  # Wider range of indigo
     'coral': (255, 127, 80),
-    'orchid': (218, 112, 214),
     'tan': (210, 180, 140),
     'azure': (240, 255, 255),
     'sienna': (160, 82, 45),
@@ -56,18 +58,15 @@ NAMED_COLORS = {
 }
 
 
-
 class FeaturesRecognition:
 
     def __init__(self):
         self.personModel = YOLO(PATH + "/dep/yolov8m.pt")
         self.keypointsModel = YOLO(PATH + '/dep/yolov8n-pose.pt')
-        rf = Roboflow(api_key="NSkF2YE2ufLzrNUiswiA")
-        project = rf.workspace().project("mask-detection-m3skq")
-        self.maskModel = project.version(1).model
+        self.maskModel = YOLO(PATH + '/dep/mask.pt')
+        self.glassesModel = YOLO(PATH + '/dep/glasses.pt')
         self.bridge = CvBridge()
-        #self.topic = "/camera/rgb/image_raw"
-        self.topic = '/usb_cam/image_raw'
+        self.topic = TOPIC
         self.rate = rospy.Rate(5)
         self.img_sub = rospy.Subscriber(self.topic,imgmsg,self.camera_callback)
         time.sleep(0.5)
@@ -76,54 +75,6 @@ class FeaturesRecognition:
 
     def camera_callback(self,data):
         self.cam_image = data
-
-
-    def funCrop(self):
-        result = None
-        while result is None:
-            frame = self.bridge.imgmsg_to_cv2(self.cam_image,desired_encoding='bgr8')
-            rospy.logwarn('Person not found... Trying again')
-            results = self.personModel.predict(frame)[0]
-            am = -100 
-            for i, box in enumerate(results.boxes):
-                if box.cls.item() == 0:
-                    cords = box.xyxy[0].tolist()
-                    area = (cords[2] - cords[0]) * (cords[3] - cords[1])
-                    if area > am:
-                        am = area
-                        xyxy = cords
-            if 'xyxy' in locals():
-                result = results[0]
-
-        rospy.loginfo('Frame set')
-        cv2.imwrite(PATH + '/data/frame.jpg', frame)    
-
-        crop = frame[int(xyxy[1]):int(xyxy[3]),int(xyxy[0]):int(xyxy[2])]       
-        self.funKeypoints(crop)
-
-
-    def zoom(self,img, zoom_factor=2):
-        return cv2.resize(img, None, fx=zoom_factor, fy=zoom_factor)
-
-
-    def funKeypoints(self,img):
-        results = self.keypointsModel(img) 
-        result = results[0]
-        result_keypoint = result.keypoints.xy.cpu().numpy()[0]
-        ombro = int(result_keypoint[5][1])
-        quadril = int(result_keypoint[11][1])
-        nareba = int(result_keypoint[0][0])      
-
-        shirt = img[ombro+10:quadril-40,30:-30]
-        pants = img[quadril+25:,:]
-        height, width = pants.shape[:2] 
-        pants = pants[:int(height/3),30:int(width/2)+10]
-        head = img[:ombro,10:-10]
-        head = self.zoom(head,3)
-        cv2.imwrite(PATH +'/data/head.jpg',head)
-        cv2.imwrite(PATH +'/data/pants.jpg',pants)
-        cv2.imwrite(PATH +'/data/shirt.jpg',shirt)
-        self.saturation(PATH + '/data/shirt.jpg')
 
 
     def saturation(self,path):
@@ -154,14 +105,11 @@ class FeaturesRecognition:
 
     def rgb_to_color_name(self,rgb):
         try:
-            # Normalizar os valores RGB para o intervalo [0, 255]
             r, g, b = [int(x) for x in rgb]
-            # Encontrar a cor mais próxima na lista predefinida
             color_name = self.closest_color((r, g, b))
             return color_name
         except ValueError:
-            return "Cor inválida"            
-            
+            return "Cor inválida"                
 
 
     def funColor(self,path):
@@ -176,48 +124,78 @@ class FeaturesRecognition:
         
 
     def ifGlasses(self,path):
-        detector = dlib.get_frontal_face_detector()
-        predictor = dlib.shape_predictor(PATH + '/dep/shape_predictor_68_face_landmarks.dat')
-        img = dlib.load_rgb_image(path)
-        rect = detector(img)[0]
-        sp = predictor(img, rect)
-        landmarks = np.array([[p.x, p.y] for p in sp.parts()])
-        nose_bridge_x = []
-        nose_bridge_y = []
-        for i in [28,29,30,31,33,34,35]:
-            nose_bridge_x.append(landmarks[i][0])
-            nose_bridge_y.append(landmarks[i][1])
+        img = cv2.imread(path)
+        results = self.glassesModel.predict(img, stream=True)     
+        for result in results:                                        
+            boxes = result.boxes.cpu().numpy()  
+            print('box:',boxes.cls)
 
-        ### x_min and x_max
-        x_min = min(nose_bridge_x)
-        x_max = max(nose_bridge_x)### ymin (from top eyebrow coordinate),  ymax
-        y_min = landmarks[20][1]
-        y_max = landmarks[31][1]
-        img2 = Image.open(path)
-        img2 = img2.crop((x_min,y_min,x_max,y_max))
-        img_blur = cv2.GaussianBlur(np.array(img2),(3,3), sigmaX=0, sigmaY=0)
-        edges = cv2.Canny(image =img_blur, threshold1=100, threshold2=200)
-        #cv2.imwrite('/home/bibo/catkin_fodase/src/Feature_extraction/features_pkg/src/data/test.jpg', edges)
-
-        #center strip
-        edges_center = edges.T[(int(len(edges.T)/2))]
-
-        if 255 in edges_center:
-            return ' '
-        else:
-            return 'Not' 
+            try:
+                box = boxes.cls[0]
+                return ' '                
+            except:
+                return ' not '
 
 
     def ifMask(self,path):
-        try:
-            results = self.maskModel.predict(path, confidence=9, overlap=30).json()
-            if results.get('predictions')[0].get('class') != 'Nomask':
-                return ' '
+        img = cv2.imread(path)
+        results = self.maskModel.predict(img, stream=True)                
+        for result in results:                                        
+            boxes = result.boxes.cpu().numpy()  
+            if boxes.cls[0] != 0:
+                return ' '                
             else:
-                return 'Not'
-        except:
-            return 'Not' #Mais provável da pessoa estar sem máscara !!!!!!! TESTAR !!!!!!!
+                return ' not '
 
+       
+    def funCrop(self):
+        result = None
+        while result is None:
+            frame = self.bridge.imgmsg_to_cv2(self.cam_image,desired_encoding='bgr8')
+            rospy.logwarn('Person not found... Trying again')
+            results = self.personModel.predict(frame)[0]
+            am = -100 
+            for i, box in enumerate(results.boxes):
+                if box.cls.item() == 0:
+                    cords = box.xyxy[0].tolist()
+                    area = (cords[2] - cords[0]) * (cords[3] - cords[1])
+                    if area > am:
+                        am = area
+                        xyxy = cords
+            if 'xyxy' in locals():
+                result = results[0]
+
+        rospy.loginfo('Frame set')
+        cv2.imwrite(PATH + '/data/frame.jpg', frame)    
+
+        crop = frame[int(xyxy[1]):int(xyxy[3]),int(xyxy[0]):int(xyxy[2])]     
+        cv2.imwrite(PATH + '/data/crop.jpg', crop)    
+  
+        self.funKeypoints(crop)
+
+
+    def zoom(self,img, zoom_factor=2):
+        return cv2.resize(img, None, fx=zoom_factor, fy=zoom_factor)
+
+
+    def funKeypoints(self,img):
+        results = self.keypointsModel(img) 
+        result = results[0]
+        result_keypoint = result.keypoints.xy.cpu().numpy()[0]
+        ombro = int(result_keypoint[5][1])
+        quadril = int(result_keypoint[11][1])
+        nareba = int(result_keypoint[0][0])      
+
+        shirt = img[ombro+10:quadril-40,50:-50]
+        pants = img[quadril+25:,:]
+        height, width = pants.shape[:2] 
+        pants = pants[:int(height/3),30:int(width/2)+10]
+        head = img[:ombro,10:-10]
+        head = self.zoom(head,3)
+        cv2.imwrite(PATH +'/data/head.jpg',head)
+        cv2.imwrite(PATH +'/data/pants.jpg',pants)
+        cv2.imwrite(PATH +'/data/shirt.jpg',shirt)
+        self.saturation(PATH + '/data/shirt.jpg')
 
     def features(self):
 
@@ -227,7 +205,7 @@ class FeaturesRecognition:
         glasses = self.ifGlasses(PATH + '/data/head.jpg')
         mask = self.ifMask(PATH + '/data/head.jpg')
 
-        out= f'I really like your {pantscolor} pants and your {shirtcolor} shirt! I see youre {glasses} wearing glasses. And youre {mask} wearing a mask.'
+        out= f'I really like your {pantscolor} pants and your {shirtcolor} top! I see youre{glasses}wearing glasses. And youre{mask}wearing a mask.'
 
         return out
 
